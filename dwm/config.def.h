7781c7a4f0bccbfc70dc83fabac6e6cfd3105ec6
Andreas Malmqvist <andreasmalmqvist1992@gmail.com>
2021-02-10 12:36:04 +0100

    Changed font

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -1,11 +1,11 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 0;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "CaskaydiaCove Nerd Font Mono:style=Bold:size=12" };
+static const char *fonts[]          = { "JetBrains Mono:style=Medium:size=14" };
 
 static const char col_bg[]			= "#e6e6e6";
 static const char col_fg[] 			= "#5E6C76";
