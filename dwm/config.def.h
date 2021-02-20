f27a2b7147b86fa0423b9b04513278e23bcd68d1
Andreas Malmqvist <andreasmalmqvist1992@gmail.com>
2021-02-20 08:58:20 +0100

    Application rules

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -30,6 +30,8 @@ static const Rule rules[] = {
 	 */
 	/* class      		instance    title       tags mask     iscentered   isfloating   monitor */
 	{ "st-256color", 	NULL, 	  	NULL, 	 	0, 			  1,			0, 			  -1 },
+	{ "st-floated", 	NULL, 	  	NULL, 	 	0, 			  1,			1, 			  -1 },
+	{ "Yad", 			NULL, 	  	NULL, 	 	0, 			  1,			1, 			  -1 },
 	{ "Gimp",     		NULL,     	NULL,       0,            0,           1,           -1 },
 	{ "Firefox",  		NULL,       NULL,       1 << 8,       0,           0,           -1 },
 };
